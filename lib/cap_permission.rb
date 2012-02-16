95566f7b72ecb9fd7435ec62f962174de1daa421
Dru Ibarra <Druwerd@gmail.com>
2012-02-15 17:14:05 -0800

    adding "deployment_users" check

--- lib/cap_permission.rb
+++ lib/cap_permission.rb
@@ -17,10 +17,15 @@ Capistrano::Configuration.instance(true).load do
      return false
   end
   
+  def deployment_user?()
+     user = EVN["SUDO_USER"]
+     deployment_users.to_a.include?(user)
+  end
+  
   namespace :permission do
      desc "check user's group membership"
      task :check do
-        if (not admin_user? and not user_in_group?(group_name))
+        if (not admin_user? and not user_in_group?(group_name) and not deployment_user?)
            abort "\n\n\n\e[0;31m You do not have proper group membership to run this deployment!  \e[0m\n\n\n"
         end
      end
